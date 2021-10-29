2280257fe6217cd3fe6fab7c05cbb25728720249
Agustin Alba Chicar <ag.albachicar@ekumenlabs.com>
2021-10-29 13:56:42 -0300

    Reduce dependencies of analysis (#9)
    * Removes simulator_gflags.cc/h
    
    * Removes simulator_config_functions.cc/h
    
    * Removes monte_carlo.cc/h
    
    * Removes simulator.cc/h
    
    * Removes simulator_config.cc/h
    
    * Removes simulator_status.cc/h
    
    * Removed all the integrators different from RK3.
    
    * Adds list of removed files to the README.md
    
    Running `time colcon build --packages-select maliput_drake` on
    this branch yields:
    
    ```sh
    time colcon build --packages-select maliput_drake
    ...
    Summary: 1 package finished [3min 3s]
      1 package had stderr output: maliput_drake
    
    real    3m3.806s
    user    18m27.039s
    sys     0m36.838s
    ```
    
    whereas in main branch:
    
    ```sh
    time colcon build --packages-select maliput_drake
    ...
    Summary: 1 package finished [4min 0s]
      1 package had stderr output: maliput_drake
    
    real    4m0.630s
    user    21m50.783s
    sys     0m43.339s
    ```
    
    * Remove gflags dependency of analysis.

--- maliput_drake/src/systems/analysis/runge_kutta2_integrator.cc
+++
@@ -1,4 +0,0 @@
-#include "maliput/drake/systems/analysis/runge_kutta2_integrator.h"
-
-DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
-    class maliput::drake::systems::RungeKutta2Integrator)
--- maliput_drake/src/systems/analysis/simulator_config.cc
+++
@@ -1,1 +0,0 @@
-#include "maliput/drake/systems/analysis/simulator_config.h"
